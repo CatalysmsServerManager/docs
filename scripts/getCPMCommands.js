6f82466ea045e82e0acc1c8b899b549d37bae1e4
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2021-01-01 11:54:26 +0100

    Add aliases to the command table

--- scripts/getCPMCommands.js
+++ scripts/getCPMCommands.js
@@ -13,7 +13,17 @@ main();
 
 async function main() {
   const data = await SdtdApi.getAllowedCommands(sdtdServer);
-  const commands = data.commands.filter(_ => _.command.startsWith('cpm-'))
+  const commands = data.commands
+    // We are only interested in CPM commands 
+    .filter(_ => _.command.startsWith('cpm-'))
+    // We add the non-prefixed alias of the command
+    // This helps our search understand stuff better
+    .map(_ => {
+      _.command += `, ${_.command.replace('cpm-', '')}`
+      return _
+    })
+
   console.log(commands);
+
   fs.writeFileSync('docs/.vuepress/public/assets/cpmCommands.json', JSON.stringify(commands))
 }
