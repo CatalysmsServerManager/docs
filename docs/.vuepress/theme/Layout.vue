a2c3d81f152ca29c8de32fcbd7a5483a0f0ea584
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2025-07-03 18:08:53 +0200

    fix: implement Takaro banner using VuePress theme extension
    - Remove problematic enhanceApp.js that used DOM manipulation
    - Create proper theme extension of vuepress-theme-yuu
    - Add Layout.vue that includes TakaroBanner component
    - This approach works with VuePress SSR and build process

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="global-layout">
+    <TakaroBanner />
+    <component :is="layout"/>
+  </div>
+</template>
+
+<script>
+import TakaroBanner from '../components/TakaroBanner.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    TakaroBanner
+  },
+  computed: {
+    layout () {
+      if (this.$page.path) {
+        if (this.$frontmatter.layout) {
+          // You can also check whether layout exists first as the default global layout does.
+          return this.$frontmatter.layout
+        }
+        return 'Layout'
+      }
+      return 'NotFound'
+    }
+  }
+}
+</script>
