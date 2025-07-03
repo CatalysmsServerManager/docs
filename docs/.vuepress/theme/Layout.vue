ffa479447213d5a7acbed84be7cb6c69ced5e077
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2025-07-03 18:28:28 +0200

    fix: resolve infinite recursion in Layout.vue
    - Import parent theme Layout using @parent-theme alias
    - Use ParentLayout component instead of string reference
    - Fixes JavaScript heap out of memory build error

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,29 +1,19 @@
 <template>
   <div id="global-layout">
     <TakaroBanner />
-    <component :is="layout"/>
+    <ParentLayout />
   </div>
 </template>
 
 <script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
 import TakaroBanner from '../components/TakaroBanner.vue'
 
 export default {
   name: 'Layout',
   components: {
+    ParentLayout,
     TakaroBanner
-  },
-  computed: {
-    layout () {
-      if (this.$page.path) {
-        if (this.$frontmatter.layout) {
-          // You can also check whether layout exists first as the default global layout does.
-          return this.$frontmatter.layout
-        }
-        return 'Layout'
-      }
-      return 'NotFound'
-    }
   }
 }
 </script>
